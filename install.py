1b696deb9061bb47f41f83b8097a84b06022f5fa
Mr. Onion <70280749+mronion420@users.noreply.github.com>
2022-07-02 20:58:03 +0600

    Update install.py

--- install.py
+++ install.py
@@ -1,6 +1,6 @@
 import os
 os.system('clear')
 print("\033[1;32;40mall recommended packages are installing be patients...\n")
-os.system('pip install colorama')
-os.system('pip install google')
+os.system('pip2 install colorama')
+os.system('pip2 install google')
 os.system('python2 uc.py -h')
